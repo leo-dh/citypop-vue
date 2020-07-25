0fad22b2cd77400a0d7dc524280be12261c0a824
Leo Ding Hao <leo.ding.hao.2014@gmail.com>
2020-07-25 21:21:18 +0800

    Changed carousel to scrollbar for AlbumModal component Added styling for AlbumModal component

--- src/components/AlbumModal.vue
+++ src/components/AlbumModal.vue
@@ -3,13 +3,27 @@
     <v-card class="maroon">
       <div class="albumModal__content">
         <img :src="$store.state.selectedAlbum.cover" alt="" class="albumModal__content__image " />
-        <div>
-          <div class="albumModal__content__details">
-            <h1>{{ $store.state.selectedAlbum.title }}</h1>
-            <h5 class="white--text">{{ $store.state.selectedAlbum.artist }}</h5>
+        <div style="height: 300px">
+          <div class="albumModal__content__details mb-9">
+            <h1 class="albumModal__content__details__title">
+              {{ $store.state.selectedAlbum.title }}
+            </h1>
+            <h5 class="albumModal__content__details__artist white--text">
+              {{ $store.state.selectedAlbum.artist }}
+            </h5>
           </div>
           <div class="albumModal__content__tracks">
-            <v-carousel hide-delimiter-background :show-arrows="false" height="250">
+            <vue-custom-scrollbar class="scroll-area" :settings="scrollBarOptions">
+              <div
+                v-for="(track, j) in $store.state.selectedAlbum.tracks"
+                :key="j"
+                class="albumModal__content__tracks__details"
+              >
+                <p>{{ track.title }}</p>
+                <p>{{ track.duration }}</p>
+              </div>
+            </vue-custom-scrollbar>
+            <!-- <v-carousel hide-delimiter-background :show-arrows="false" height="250">
               <v-carousel-item
                 v-for="(trackSplit, i) in trackSplits"
                 :key="i"
@@ -25,7 +39,7 @@
                   <p>{{ track.duration }}</p>
                 </div>
               </v-carousel-item>
-            </v-carousel>
+            </v-carousel> -->
           </div>
         </div>
       </div>
@@ -45,34 +59,81 @@
 
 <script lang="ts">
 import Vue from "vue";
+import vueCustomScrollbar from "vue-custom-scrollbar";
+import { Track } from "@/types/model";
 
 export default Vue.extend({
   name: "AlbumModal",
+  components: {
+    vueCustomScrollbar
+  },
   data() {
-    return {};
+    return {
+      scrollBarOptions: {
+        suppressScrollX: true,
+        minScrollbarLength: 60
+      }
+    };
   },
   computed: {
     trackSplits() {
       const tracks = this.$store.state.selectedAlbum.tracks;
       const chunk = 5;
-      const chunks = [];
-      let i, j;
-      for (i = 0, j = tracks.length; i < j; i += chunk) {
-        chunks.push(tracks.slice(i, i + chunk));
+      let splits: Track[] = [];
+      for (let i = 0; i < tracks.length; i += chunk) {
+        splits = [...splits, tracks.splice(i, i + chunk)];
       }
-      return chunks;
+      return splits;
     }
   }
 });
 </script>
 
 <style lang="scss">
+.scroll-area {
+  position: relative;
+  margin: auto;
+  width: auto;
+  overflow-x: hidden;
+  height: 100%;
+}
+.ps .ps__rail-y:hover,
+.ps .ps__rail-y:focus,
+.ps .ps__rail-y.ps--clicking {
+  opacity: 0.9;
+  background-color: transparent;
+}
+.ps__thumb-y {
+  opacity: 0.3;
+  transition: background-color 0.2s linear, width 0.2s ease-in-out, opacity 0.2s linear;
+}
+.ps__rail-y:hover > .ps__thumb-y,
+.ps__rail-y:focus > .ps__thumb-y,
+.ps__rail-y.ps--clicking .ps__thumb-y {
+  background-color: $color-orange;
+  opacity: 1;
+}
+
+.ps__rail-y {
+  opacity: 0.6;
+}
+
 .albumModal {
   border-radius: 15px !important;
   &__content {
     display: flex;
-    padding: 2em;
+    padding: 80px 80px 48px;
     align-items: center;
+    &__details {
+      &__title {
+        font-size: 3em;
+        line-height: 1.25em;
+      }
+      &__artist {
+        font-size: 1.25em;
+        line-height: 1.2em;
+      }
+    }
     &__image {
       width: 300px;
       height: 300px;
@@ -81,9 +142,10 @@ export default Vue.extend({
       margin-right: 60px;
     }
     &__tracks {
-      height: 200px;
+      height: 180px;
       &__details {
-        min-width: 250px;
+        min-width: 300px;
+        padding-right: 2em;
         display: flex;
         justify-content: space-between;
       }
